c30694b549dbc50ee0e45fcc8222383eb3f0a762
Kamil Kurp <kam.kurp@gmail.com>
2019-01-07 11:46:16 +0100

    point 10?

---
+++ src/main/java/pl/edu/agh/gg/productions/P6.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.gg.productions;
+
+import pl.edu.agh.gg.HyperGraph;
+import pl.edu.agh.gg.domain.VertexLike;
+
+import java.awt.image.BufferedImage;
+
+public class P6 extends Production {
+    public P6(BufferedImage image, HyperGraph graph) {
+        super(image, graph);
+    }
+
+    @Override
+    public void apply(VertexLike center) throws CannotApplyProductionException {
+
+    }
+}
